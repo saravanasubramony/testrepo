c5aec0b5621763e120d9a73f37f5d2914a6cd816
Saravana Subramony <saravanasubramony@gmail.com>
2018-08-22 21:49:51 -0400

    initail commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Saravana Kumar Subramony on 8/19/18.
+//  Copyright © 2018 Saravana Kumar Subramony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //optional or not optioanal
+    var name:String? = ""
+ 
+    
+    @IBOutlet weak var lblName: UILabel!
+    
+    @IBAction func btnSubmit(_ sender: UIButton) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+
+
+}
+
